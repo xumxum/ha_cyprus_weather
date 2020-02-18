f45f35bad01ed171e55b420ae04baf36edaec0b6
zsolt <zsolt@u-tx.com>
2020-02-18 17:43:56 +0200

    working on getting forecast data

--- cyprus_weather_org.py
+++ cyprus_weather_org.py
@@ -43,20 +43,57 @@ def getData(url):
 
     weatherData["Current.UVIndex"] = re.compile('<th>UV Index:</th>\s+<td>(.+?)</td>').findall(currentDetails_s)[0]
     
-    #forecast_s = str(cwMain.find_all("div",class_="hourly")[0])
+    #Hourly forecast data
     forecasts_v = cwMain.find_all("div",class_="hour")
+    forecast_hourly={}
     for forecast_entry_s in forecasts_v:
-        pprint(forecast_entry_s)
-        h = re.compile('>\s*(.+?)\s*<').findall(str(forecast_entry_s))[0]
-        pprint(h)
-        d = re.compile('/>\r\n\s+(\d+).+\r\n').findall(str(forecast_entry_s))[0]
+        #pprint(forecast_entry_s)
+        hourly_forecast_time = re.compile('>\s*(.+?)\s*<').findall(str(forecast_entry_s))[0]
+        #pprint(hourly_forecast_time)
+        hourly_forecast_temperature = re.compile('/>\r\n\s+(\d+).+\r\n').findall(str(forecast_entry_s))[0]
         #d = re.compile('>\s*(.+?)\s*<').findall(str(forecast_entry_s))[0]
-        pprint(d)
-    #pprint(h)
+        #pprint(hourly_forecast_temperature)
+        #forecast_hourly.append()
+        forecast_hourly[hourly_forecast_time] = hourly_forecast_temperature
+    #pprint(forecast_hourly)
+    
+    weatherData["Forecast.Hourly"] = forecast_hourly
+    
+    #Today forecast
+    #<div class="day-forecast">
+    today_forecast = cwMain.find_all("div",class_="forecast thisDay")[0]
+    #first is daytime, 2nd is nighttime
+    today_forecast_periods = today_forecast.find_all("div",class_="period") 
+    prefix_v=['Day', 'Night']
+    for i in range(2):
+        forecast_s = str(today_forecast_periods[i])
+        prefix = prefix_v[i]
+                
+        weatherData["Forecast." + prefix +".Description"] = re.compile('<div class="description">(.+?)<').findall(forecast_s)[0]
+        weatherData["Forecast." + prefix +".ChanceOfRain"] = re.compile('Chance of Rain: (\d+)%').findall(forecast_s)[0]
+        weatherData["Forecast." + prefix +".Wind"] = re.compile('(\d+) km\/h').findall(forecast_s)[0]
 
-    #weatherData["Current.Condition"]="windy"
-
+    weatherData["Forecast." + prefix_v[0] + ".TempHigh"] = re.compile('<div class="temp temp-high">(\d+)').findall( str(today_forecast_periods[0]) )[0]
+    weatherData["Forecast." + prefix_v[1] + ".TempLow"] = re.compile('<div class="temp temp-low">(\d+)').findall( str(today_forecast_periods[1]) )[0]
     #pprint(weatherData)
+    
+    #dayly_forecast = cwMain.find_all("div",class_="forecast")[0]
+    #re.compile("itl")
+    dayly_forecast = cwMain.find_all("div",class_="forecast" )[1] #first is today forecast, ignore, was already processed 
+    
+    #pprint(str(dayly_forecast))
+    
+    day_container = dayly_forecast.find_all("div",class_="day-container")
+
+    forecast_dayly={}
+    for day_container_entity in day_container:
+        #day-date
+        #pprint(day_container_entity)
+        day_date_s = str( day_container_entity.find_all("div",class_="day-date")[0] )
+        f_date = re.compile('<span>(.+?)<').findall(day_date_s)[0]
+        
+        pprint(f_date)
+
 
     return weatherData
 
